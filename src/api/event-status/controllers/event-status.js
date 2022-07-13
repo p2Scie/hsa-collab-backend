6f527c5fd5b8c6798cda455d963b2b62b2ba078d
Stevens <sdemervil@gmail.com>
2022-07-13 11:36:30 +0200

    update content-type

--- src/api/event-status/controllers/event-status.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  event-status controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::event-status.event-status');
--- src/api/event-status/services/event-status.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * event-status service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::event-status.event-status');
